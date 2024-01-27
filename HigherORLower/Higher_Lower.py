85f48acb2790044b0b47c1c45bbd1361d901f024
Sam <101752847+Samade11@users.noreply.github.com>
2024-01-27 21:25:47 +0000

    Add files via upload

---
+++ HigherORLower/Higher_Lower.py
@@ -0,0 +1,57 @@
+#Higher_Lower
+from Higher_Art import logo, vs , data
+import random 
+import os
+
+
+print(logo)
+score = 0
+game_should_continue = True
+account_b = random.choice(data)
+
+while game_should_continue:
+    def format_data(account):
+        """Format accounts data and returns the printable format"""
+        account_name = account["name"]
+        account_descr = account["description"]
+        account_country = account["country"]
+        return f"{account_name}, a {account_descr}, from {account_country}"
+
+    def check_answer(guess, a_followers, b_followers):
+        """Use if statement to check is user is correct."""
+        if a_followers > b_followers:
+            return guess == "a"
+        else:
+            return guess == "b"
+
+    #Generate random account
+    import random
+    account_a = account_b
+    account_b = random.choice(data)
+    while account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+
+    #Ask user for guess
+    guess = input("Who has more followers type 'A' or 'B': ").lower()
+
+    #Check if user is correct
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    #CLEAR FUNCTION
+    os.system('cls')
+    print(logo)
+    
+    
+    ##Score keeping
+    if is_correct:
+        score += 1
+        print(f"That's correct. Current score: {score}")
+    else:
+        game_should_continue = False
+        print(f"Sorry, that's wrong. Final score: {score}")
